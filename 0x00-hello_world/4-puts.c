8056371ae552e516a6965d77bfa57943f013dede
root <isaiah.abiodun@outlook.com>
2022-03-10 11:13:00 -0800

    Printing with the put method in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *	main - print the string in put function
+ *	Description: this function print the string in put function
+ *	Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
