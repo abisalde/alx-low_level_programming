845cbae938ad087222dbf0a0af67c72d55012dea
root <isaiah.abiodun@outlook.com>
2022-03-10 11:38:28 -0800

    Printing string with Printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *	main - Program that prints a string with printf
+ *	Description: A porgram prints a string with printf
+ *	followed by a new line
+ *	Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
