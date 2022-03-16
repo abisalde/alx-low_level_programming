f8f9c4496ffef3b433a9e55b37d56a1afa4d1d36
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2022-03-16 18:27:53 +0000

    changes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,12 +9,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
 
 	return (0);
 }
