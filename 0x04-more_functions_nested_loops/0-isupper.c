6fd35d1243dc65f9b5d1472a1ba8c3737c30b3b9
root <isaiah.abiodun@outlook.com>
2022-03-17 12:44:43 -0700

    check if it is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,11 @@
 
 /**
  * _isupper - functions that checks for uppercase character
+ * @c: character being checked
  * Return: 1 if c is uppercase and 0 otherwise
  */
 
 int _isupper(int c)
 {
-	char c;
+	return ((c >= 'A' && c <= 'Z'));
 }
