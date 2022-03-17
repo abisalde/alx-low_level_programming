de8b6cfbd153993bf8f2d6ae2adfd9d97e10fdec
root <isaiah.abiodun@outlook.com>
2022-03-17 12:57:06 -0700

    check for digits 0 - 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,10 +3,10 @@
 /**
  * _isdigit - check for digit from 0 to 9
  * @c: character to be checked
- * Return: 0 (1)
+ * Return: 1 if c is uppercase or 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	
+	return ((c >= '0' && c <= '9'));
 }
