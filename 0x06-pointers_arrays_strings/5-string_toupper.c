d0657ef69d04c0927a0dcd8faceb9c5ff8e751a6
root <isaiah.abiodun@outlook.com>
2022-03-24 20:31:25 -0700

    reverse toUppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* string_toupper - converts all lowercase letters of a string to uppercase
+* @str: string to be comverted
+*
+* Return: pointer to converted uppercase string
+*/
+char *string_toupper(char *str)
+{
+	char *new_str = str;
+
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			*str = *str - 32;
+
+		str++;
+	}
+
+	return (new_str);
+}
+
