dfc75c62d8accdd041eeac007f81f52673fa54f2
root <isaiah.abiodun@outlook.com>
2022-04-01 17:27:06 -0700

    program that prints it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by a new line
+ * @argc: integer
+ * @argv: the list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
