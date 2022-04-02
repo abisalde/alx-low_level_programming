d3ea84985c87daf337df784b9b7fc863eb6c42a0
root <isaiah.abiodun@outlook.com>
2022-04-01 17:37:06 -0700

    prints the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,11 @@
  * Return: (0)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%d\n", argc);
+	(void)argv;
+
+	printf("%i\n", argc - 1);
 
 	return (0);
 }
