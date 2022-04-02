8ef664902d23355b9a1d47432e939ed36ae43b2d
root <isaiah.abiodun@outlook.com>
2022-04-01 17:52:33 -0700

    prints all arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+
+/**
+ * main - program that print all arguments it receive
+ * @argc: integer
+ * @argv: the list
+ * Return: (0)
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", i, argv[i]);
+	}
+
+	return (0);
+}
