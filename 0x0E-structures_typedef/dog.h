b56dc0f83241dff1405ff1a65f82b35daea9b96c
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2022-04-12 23:49:06 +0000

    testing dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,12 @@
  * @owner: character string
  */
 
-/**
- * dog_t - typedef struct dog
- */
-
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
